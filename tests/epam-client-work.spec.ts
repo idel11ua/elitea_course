bc5f19401b582241727a6978bb2834b942f94214
idel11ua <idel11@ukr.net>
2026-03-25 11:24:07 +0200

    Create tests/epam-client-work.spec.ts

---
+++ tests/epam-client-work.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+// Test: EPAM - Verify Client Work page
+// Steps:
+// 1. Navigate to https://www.epam.com/
+// 2. Select "Services" from the header menu
+// 3. Click the "Explore Our Client Work" link
+// 4. Verify that the "Client Work" text is visible on the page
+
+// Initial scaffold - test body will be added in the next edit
+
+test('EPAM - Client Work flow (scaffold)', async ({ page }) => {
+  // TODO: implement test steps
+});
